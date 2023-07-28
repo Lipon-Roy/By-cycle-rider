1610563dda619bc3ce77f7633c0c17f9064883e4
Lipon Roy <royliponjkkniu@gmail.com>
2023-07-28 20:12:49 +0600

    just get method include for testing purpose

--- routers/cycleRouter.js
+++ routers/cycleRouter.js
@@ -3,6 +3,10 @@ const cycleController = require('../controllers/cycleController');
 
 const router = express.Router();
 
+router.get('/', (req, res) => {
+    res.send("Hello By cycle Rider");
+});
+
 router.post('/', cycleController);
 
 module.exports = router;
